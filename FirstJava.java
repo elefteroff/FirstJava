6b43afc444b02012d996ab71c423bf8acdc02692
elefteroff <elefteroff@mac.com>
2022-02-05 11:17:10 -0800

    Initial Commit

---
+++ FirstJava.java
@@ -0,0 +1,7 @@
+public class FirstJava {
+	public static void main(String[] args) {
+		System.out.println("My Name is Eric.");
+        System.out.println("I am 25 (I wish!) years old.");
+        System.out.println("My hometown is in the United States.");
+	}
+}
